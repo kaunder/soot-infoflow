24aa9ac104ac253e593c7b6d51860da1fc6e9719
kaunder <kaunder@ucalgary.ca>
2015-10-15 23:51:07 -0600

    add output of flow fxn type, abstraction source context, abstraction access path for each abstraction

--- src/soot/jimple/infoflow/sgp/SGPHandler.java
+++ src/soot/jimple/infoflow/sgp/SGPHandler.java
@@ -11,6 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Set;
 
 import soot.Unit;
@@ -68,15 +69,42 @@ public class SGPHandler implements TaintPropagationHandler{
     public void notifyFlowIn(Unit stmt, Abstraction taint, IInfoflowCFG cfg,
 			     FlowFunctionType type) {
 	//System.out.println("I am calling from notifyFlowIn, a taint is being propagated!");
-	String handler = "Handler: notifyFlowIn\n";
-	String flwfxn = type.toString();
+    
+    //Set up strings to hold Abstraction info
+    String flwfxn;	
+    String acspath;
+    String srcctxt;
+    
+    //Identify name of handler function for the log
+    String handler = "Handler: notifyFlowIn\n";
+	
+	//Get flow function type
+	if(type!=null){
+		flwfxn = type.toString();
+	}else{
+		flwfxn="null";
+	}
+	
+	//Get tainted access path
+	if((taint.getAccessPath()!=null)&&(taint.getAccessPath().getPlainValue()!=null)){
+		acspath = taint.getAccessPath().getPlainValue().toString();	
+	}else{
+		acspath="null";
+	}
+	
+	//Get source context
+	if(taint.getSourceContext()!=null){
+		srcctxt = taint.getSourceContext().toString();
+	}else{
+		srcctxt="null";
+	}
+	
+	//Append log info, pass appended strings to log function for output	
 	String flow = "Flow function type: "+flwfxn+"\n";
-	String acspath = taint.getAccessPath().getPlainValue().toString();
 	String accesspath = "AccessPath value: "+acspath+"\n";    
-	String srcctxt = taint.getSourceContext().toString();
 	String sourcecontext = "Source Context: "+srcctxt+"\n";
 	
-		this.log(handler+flow+accesspath+sourcecontext);
+	this.log(handler+flow+accesspath+sourcecontext);
 		
 	}
 
@@ -85,13 +113,103 @@ public class SGPHandler implements TaintPropagationHandler{
 	public Set<Abstraction> notifyFlowOut(Unit stmt, Abstraction d1,
 			Abstraction incoming, Set<Abstraction> outgoing, IInfoflowCFG cfg,
 			FlowFunctionType type) {
-		// TODO Auto-generated method stub
-		System.out.println("I am calling from notifyFlowOut, a new taint has been created!");
-		YOU ARE HERE - modify the log so that it operates as above but shows the difference in incoming and outgoing Abstr
-		this.log("I am calling from notifyFlowOut, a new taint has been created! ");
-		return outgoing; //pass on outgoing abstraction without altering
+		
+		//Identify name of handler function for the log
+		String handler = "Handler: notifyFlowOut\n";
+		
+		//Set up strings for log file text
+		String inabs = "***Incoming Abstraction Data***\n";
+		String outabs="***Outgoing Abstraction Set Data***\n";
+		String acspathOUTtxt="Outgoing Abstraction Access Path: ";
+		String srcctxtOUTtxt="Outgoing Abstraction Source Context: ";
+		
+		//Set up strings for getting Abstraction data
+		String flwfxn;
+		String acspathIN;
+		String srcctxtIN;
+		String acspathOUT;
+		String srcctxtOUT;
+		
+		//Get type of flow function
+		if(type!=null){
+			flwfxn = type.toString();
+		}else{
+			flwfxn="null";
+		}
+		
+		
+		//Get incoming access path
+		if((incoming.getAccessPath()!=null)&&(incoming.getAccessPath().getPlainValue()!=null)){
+			acspathIN = incoming.getAccessPath().getPlainValue().toString();
+		}else{
+			acspathIN="null";
+		}
+		
+		//Get incoming source context
+		if(incoming.getSourceContext()!=null){
+			srcctxtIN = incoming.getSourceContext().toString();
+		}else{
+			srcctxtIN="null";
+		}
+
+		
+		//Get information about all outgoing Abstractions
+		Iterator<Abstraction> outIter;
+		if(outgoing!=null&&!outgoing.isEmpty()){
+		    outIter = outgoing.iterator();
+
+		    int j=0; //loop counter
+		    Abstraction temp; //temp object for loop access
+		    acspathOUT=null; //initialize strings
+		    srcctxtOUT=null;
+		    
+
+		    //loop over all Abstractions in outgoing set
+		    while(outIter.hasNext()){
+			outabs=outabs.concat("Outgoing set "+j+":\n");
+
+			//Advance iterator to next element in set
+			temp=outIter.next();
+
+			//Get AccessPath from this Abstraction
+			if((temp.getAccessPath()!=null)&&(temp.getAccessPath().getPlainValue()!=null)){
+				acspathOUT = temp.getAccessPath().getPlainValue().toString();
+			}else{
+				acspathOUT="null";
+			}
+			
+			//Get SourceContext from this Abstraction
+			if(temp.getSourceContext()!=null){
+				srcctxtOUT = temp.getSourceContext().toString();
+			}else{
+				srcctxtOUT="null";
+			}
+			
+			//Build output string
+			outabs=outabs.concat(acspathOUTtxt+acspathOUT+"\n"+srcctxtOUTtxt+srcctxtOUT+"\n");
+			
+			j++;
+		    }
+		}else{
+			acspathOUT="null";
+			srcctxtOUT="null";
+		    outabs=outabs.concat("Outgoing set empty - taint not propagated.\n");
+		}
+
+		//Append collected info with description strings
+		String flow = "Flow function type: "+flwfxn+"\n";
+		String accesspathIN = "Incoming AccessPath value: "+acspathIN+"\n";
+		String sourcecontextIN = "Incoming Source Context: "+srcctxtIN+"\n";
+		
+		//Send full output string to log
+		this.log(handler+flow+inabs+accesspathIN+sourcecontextIN+outabs+"\n");
+		
+		return outgoing; //pass on outgoing abstraction set without altering
 	}
 
+	
+	
+	
     /*Write to the log file*/
     private void log(String str){
 	synchronized(foutWriter){ //ensure safe concurrent access
